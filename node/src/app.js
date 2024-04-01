c946475cdeca39c9b38299efaa3485b0641353e4
micaela andrade <micaela.andrade@luizalabs.com>
2024-03-31 21:56:51 -0300

    chore: ajuste na porta

--- node/src/app.js
+++ node/src/app.js
@@ -2,38 +2,29 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
-const db = mysql.createConnection({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'test'
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST, 
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
-db.connect();
+connection.connect();
 
 app.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  db.query('INSERT INTO people (name) VALUES ("Full Cycle Rocks!")', (error, results, fields) => {
+  connection.query('SELECT * FROM people', (error, results, fields) => {
     if (error) throw error;
 
-    db.query('SELECT * FROM people', (error, results, fields) => {
-      if (error) throw error;
-
-      let html = '<h1>Full Cycle Rocks!</h1><ul>';
-      results.forEach(row => {
-        html += `<li>${row.name}</li>`;
-      });
-      html += '</ul>';
-
-      res.send(html);
+    let names = '<ul>';
+    results.forEach(result => {
+      names += `<li>${result.name}</li>`;
     });
+    names += '</ul>';
+
+    res.send(`<h1>Full Cycle Rocks!</h1>${names}`);
   });
 });
 
 app.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log('Server is running on port 3000');
 });
